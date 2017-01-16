3bd578b69aa1d519aab90dd7d30d19388dc44b69
Benjamin Stemper <ben.m.stemper@gmail.com>
2017-01-16 19:02:43 +0100

    Initial linted commit of Modified Rough Bergomi framework.

---
+++ code/__init__.py
@@ -0,0 +1,7 @@
+
+__author__     = 'Benjamin Stemper'
+__credits__    = ['Christian Bayer', 'Peter K. Friz', 'Benjamin Stemper',
+				  'Archil Gulisashvili', 'Blanka Horvath']
+__license__    = 'MIT'
+__maintainer__ = 'Benjamin Stemper'
+__email__      = 'stemper@math.tu-berlin.de'
